cb4ae199cbcf80f24c2f50a6a3bd5cf450d6e5ea
Ralph Segi <segi@comsolit.com>
2019-01-18 14:47:48 +0100

    Add Coop component to core

---
+++ www/src/components/Coop.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="hello">
+    Welcome to COOP website. Please choose your destiny
+    <br>
+    <ul>
+      <li><router-link to="/person">To Person</router-link></li>
+      <li><router-link to="/events">To Events</router-link></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Coop',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: block;
+  margin: 10px 0;
+}
+a {
+  color: #42b983;
+}
+</style>
